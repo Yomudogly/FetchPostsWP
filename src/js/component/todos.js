844fa4093321548185cc1d0d2f295411c5321cdc
Roman Khalnepesov <khalnepesov@gmail.com>
2019-11-24 22:35:17 +0000

    add pointer

--- src/js/component/todos.js
+++ src/js/component/todos.js
@@ -7,78 +7,99 @@ export let Todos = () => {
 
 	return (
 		<div>
-			<div className="input-group mb-3">
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Type your next todo..."
-					aria-label="Type your next todo..."
-					aria-describedby="button-addon2"
-					onChange={e => setTemp(e.target.value)}
-					onKeyUp={e => {
-						//listen to the key up and wait for the return key to be pressed (KeyCode === 13)
-						if (e.keyCode === 13) {
-							setTodos(
-								[
-									{
-										text: e.target.value,
-										done: false,
-										id: Math.floor(Math.random() * 100000)
+			<h1 className="text-center mt-5 text-light">
+				{<i className="fa fa-check-square" />} To Do List...
+			</h1>
+			<div className="container d-flex justify-content-center">
+				<div
+					className="card bg-light border"
+					style={{ width: "30rem" }}>
+					<div className="card-body">
+						<div className="input-group input-group-lg card-title">
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Type your next todo..."
+								aria-label="Type your next todo..."
+								aria-describedby="button-addon2"
+								onChange={e => setTemp(e.target.value)}
+								onKeyUp={e => {
+									//listen to the key up and wait for the return key to be pressed (KeyCode === 13)
+									if (e.keyCode === 13) {
+										setTodos(
+											[
+												{
+													text: e.target.value,
+													done: false,
+													id: Math.floor(
+														Math.random() * 100000
+													)
+												}
+											].concat(todos)
+										);
+										setTemp("");
 									}
-								].concat(todos)
-							);
-							setTemp("");
-						}
-					}}
-					value={tempInputvalue}
-				/>
-				<div className="input-group-append">
-					<button
-						className="btn btn-outline-secondary"
-						type="button"
-						id="button-addon2"
-						onClick={() => {
-							setTodos(
-								[
-									{
-										text: tempInputvalue,
-										done: false,
-										id: Math.floor(Math.random() * 100000)
+								}}
+								value={tempInputvalue}
+							/>
+							<div className="input-group-append">
+								<button
+									className="btn btn-outline-secondary"
+									type="button"
+									id="button-addon2"
+									onClick={() => {
+										setTodos(
+											[
+												{
+													text: tempInputvalue,
+													done: false,
+													id: Math.floor(
+														Math.random() * 100000
+													)
+												}
+											].concat(todos)
+										);
+										setTemp("");
+									}}>
+									Button
+								</button>
+							</div>
+						</div>
+
+						<ul className="list-group list-group-flush">
+							{todos.map(t => (
+								<li
+									onClick={() =>
+										setTodos(
+											todos
+												.map(todo => {
+													if (todo.id === t.id) {
+														todo.done = !todo.done;
+													}
+
+													return todo;
+												})
+												.sort(
+													todo =>
+														todo.done > !todo.done
+															? 1
+															: -1
+												)
+										)
 									}
-								].concat(todos)
-							);
-							setTemp("");
-						}}>
-						Button
-					</button>
+									key={t.id}
+									className={
+										`list-group-item bg-light` +
+										` ` +
+										`${t.done ? "done" : "notDone"}`
+									}>
+									{t.text}
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 			</div>
-
-			<ul>
-				{todos.map(t => (
-					<li
-						onClick={() =>
-							setTodos(
-								todos
-									.map(todo => {
-										if (todo.id === t.id) {
-											todo.done = !todo.done;
-										}
-
-										return todo;
-									})
-									.sort(
-										todo =>
-											todo.done > !todo.done ? 1 : -1
-									)
-							)
-						}
-						key={t.id}
-						className={`${t.done ? "done" : "notDone"}`}>
-						{t.text}
-					</li>
-				))}
-			</ul>
 		</div>
 	);
 };
